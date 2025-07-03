d4f5f5443ac641be8d97c5fa06659b5524222be4
duongbuidz <135213865+duongbuidz@users.noreply.github.com>
2025-07-03 21:32:53 +0700

    Update DMEM.v

--- DMEM.v
+++ DMEM.v
@@ -1,8 +1,7 @@
 module DMEM (
-    input clk, rst, MemRead, MemWrite,
-    input [2:0] funct3, 
+    input clk, MemWrite,    
     input [31:0] address, write_data,
-    output reg [31:0] read_data
+    output reg [31:0] read_data    
 );
     reg [31:0] memory [0:1023]; // 256 word = 1024 byte
     always_ff @(posedge clk) begin
