a3a84378c553abeaf1d80f0cf603141812f54930
duongbuidz <135213865+duongbuidz@users.noreply.github.com>
2025-07-03 21:49:28 +0700

    Update RegisterFile.v

--- RegisterFile.v
+++ RegisterFile.v
@@ -1,5 +1,6 @@
 module RegisterFile (
-    input clk,    
+    input clk,
+    input rst, // Thêm tín hiệu reset
     input [4:0] addA, addB, addD,
     input [31:0] WB_out,
     input RegWrite,
@@ -8,7 +9,7 @@ module RegisterFile (
     logic [31:0] regs [0:31];
 
     // Khởi tạo thanh ghi khi reset
-    always @(posedge clk ) begin
+    always @(posedge clk or posedge reset) begin
         if (reset) begin
             for (int i = 0; i < 32; i++) begin
                 regs[i] <= 32'b0;
