97bc6ae8c3cb9b19795e0b94336d0b2074d9533a
duongbuidz <135213865+duongbuidz@users.noreply.github.com>
2025-07-03 21:48:38 +0700

    Update RegisterFile.v

--- RegisterFile.v
+++ RegisterFile.v
@@ -1,5 +1,5 @@
 module RegisterFile (
-    input clk,
+    input clk,    
     input [4:0] addA, addB, addD,
     input [31:0] WB_out,
     input RegWrite,
@@ -7,18 +7,19 @@ module RegisterFile (
 );
     logic [31:0] regs [0:31];
 
-    // reset về 0
-    initial begin
-        for (int i = 0; i < 32; i++) regs[i] = 32'b0;
-    end
-   
-    always(*) begin
-        dataA = regs[addA];
-        dataB = regs[addB];
-    end   
-    
-    always@(posedge clk) begin
-        if (RegWrite && addD  != 0)
+    // Khởi tạo thanh ghi khi reset
+    always @(posedge clk ) begin
+        if (reset) begin
+            for (int i = 0; i < 32; i++) begin
+                regs[i] <= 32'b0;
+            end
+        end
+        else if (RegWrite && addD != 0) begin
             regs[addD] <= WB_out;
+        end
     end
+
+    // Gán giá trị đầu ra
+    assign dataA = regs[addA];
+    assign dataB = regs[addB];
 endmodule
